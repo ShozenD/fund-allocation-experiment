ca507fb583c92e4ae09a0efcfb6ac5d8811390db
Shozen Dan <shozen.dan@gmail.com>
2019-11-05 11:39:11 -0800

    created voting table function in Table.js and edited group-vote.html

--- docs/src/Table.js
+++ docs/src/Table.js
@@ -101,3 +101,36 @@ function mainTable(myAlloc) {
   // return all elements
   return ('<br><h2 align="left">My Allocations</h2><table class="table">' + thead + tbody + '</table>')
 }
+
+function voteTable(myAlloc, leaderAlloc) {
+  var diffRp = Math.abs(myAlloc.rp - leaderAlloc.rp)
+  var diffHhp = Math.abs(myAlloc.hhp - leaderAlloc.hhp)
+  var diffFuf = Math.abs(myAlloc.fuf - leaderAlloc.fuf)
+  var diffNales = Math.abs(myAlloc.nales - leaderAlloc.nales)
+  var diffTrip = Math.abs(myAlloc.trip - leaderAlloc.trip)
+  var diffTotal = myAlloc.findTotalDiff(leaderAlloc)
+  var earnings = myAlloc.findTotal() - diffTotal
+
+  // HTML
+  var title='<br><h2 align="left">Allocation Vote</h2>'
+
+  var thead = '<thead class="thead-dark"><tr><th scope="col">Non-profit Organization</th><th scope="col">Leader`s Choice</th><th scope="col">Your Allocation</th><th scope="col">Difference</th></tr></thead>'
+
+  var rp = '<tr><th scope="row" align="left">Reading Partners</th><td class="leader-alloc" id="rp" align="center">' + leaderAlloc.rp + '</td><td class="my-alloc" id="rp" align="center">' + myAlloc.rp + '</td><td class="diff-alloc" id="rp" align="center">' + diffRp + '</td></tr>'
+
+  var hhp = '<tr><th scope="row" align="left">Human Health Partners</th><td class="leader-alloc" id="hhp" align="center">' + leaderAlloc.hhp + '</td><td class="my-alloc" id="hhp" align="center">' + myAlloc.hhp + '</td><td class="diff-alloc" id="hhp" align="center">' + diffHhp + '</td></tr>'
+
+  var fuf = '<tr><th scope="row" align="left">Friends of the Urban Forest</th><td class="leader-alloc" id="fuf" align="center">' + leaderAlloc.fuf + '</td><td class="my-alloc" id="fuf" align="center">' + myAlloc.fuf + '</td><td class="diff-alloc" id="fuf" align="center">' + diffFuf + '</td></tr>'
+
+  var nales = '<tr><th scope="row" align="left">National Alliance for Law Enforcement Support</th><td class="leader-alloc" id="nales" align="center">' + leaderAlloc.nales + '</td><td class="my-alloc" id="nales" align="center">' + myAlloc.nales + '</td><td class="diff-alloc" id="nales" align="center">' + diffNales + '</td></tr>'
+
+  var trip = '<tr><th scope="row" align="left">TRIP</th><td class="leader-alloc" id="trip" align="center">' + leaderAlloc.trip + '</td><td class="my-alloc" id="trip" align="center">' + myAlloc.trip + '</td><td class="diff-alloc" id="trip" align="center">' + diffTrip + '</td></tr>'
+
+  var total = '<tr><th scope="row" align="left">Total</th><td class="leader-alloc-total" align="center">' + leaderAlloc.findTotal() + '</td><td id="my-alloc-total" align="center">' + myAlloc.findTotal() + '</td></tr>'
+
+  var earn = '<tr><th scope="row">Your Earnings</th><td id="earnings">' + earnings + '</td><td></td><td></td></tr>'
+
+  var tbody = '<tbody>' + rp + hhp + fuf + nales + trip + total + earn + '</tbody>'
+
+  return (title + thead + tbody)
+}
