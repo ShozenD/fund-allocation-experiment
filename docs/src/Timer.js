78ce52bb951c50b0c9bd33a0f63172a0c41970af
Shozen Dan <shozen.dan@gmail.com>
2019-12-13 19:55:15 -0800

    Created timer object

---
+++ docs/src/Timer.js
@@ -0,0 +1,38 @@
+class AllocationTimer {
+  constructor (startTime) {
+    this.startTime = startTime
+    this.remainTime = 300000 // 30 mins
+    this.timeout = false
+  }
+
+  startTimer () {
+    var currentTime = new Date()
+    var timeSinceStart = this.startTime - currentTime
+    this.remainTime = this.remainTime - timeSinceStart
+    this.alertTime()
+    this.displayTime()
+    setTimeout(this.startTimer, 500)
+  }
+
+  displayTime () {
+    var minutes = Math.floor(this.remainTime / 60000)
+    var seconds = Math.floor((this.remainTime % 60000) / 1000).toFixed(0)
+    return minutes + ':' + (seconds < 10 ? '0' : '') + seconds
+  }
+
+  alertTime () {
+    const m = 1000 * 60
+    if (this.remainTime < m * 15) {
+      alert('15 minutes remaining')
+    } else if (this.remainTime < m * 10) {
+      alert('10 minutes remaining')
+    } else if (this.remainTime < m * 5) {
+      alert('5 minutes remaining')
+    } else if (this.remainTime < m * 1) {
+      alert('1 minute remaining')
+    } else if (this.remainTime < 0) {
+      alert('Timeout!')
+      this.timeout = true
+    }
+  }
+}
