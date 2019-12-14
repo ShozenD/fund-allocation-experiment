2c6d2d89bf3d129763cd85ec68e4ac5c26de8b2c
Shozen Dan <shozen.dan@gmail.com>
2019-12-14 00:51:12 -0800

    remade Timer.js

--- docs/src/Timer.js
+++ docs/src/Timer.js
@@ -1,35 +1,61 @@
-class AllocationTimer {
-  constructor (startTime) {
-    this.startTime = startTime
-    this.remainTime = 1000 * 60 * 30 // 30 mins
-    this.timeout = false
+var timeInMinutes = 30
+var currentTime = Date.parse(new Date())
+var deadline = new Date(currentTime + timeInMinutes * 60 * 1000)
+
+function getTimeRemaining (endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date())
+  var seconds = Math.floor((t / 1000) % 60)
+  var minutes = Math.floor((t / 1000 / 60) % 60)
+  return {
+    total: t,
+    minutes: minutes,
+    seconds: seconds
+  }
+}
+
+function alertTimeRemaining (t) {
+  var m = t.minutes
+  if (m === 15) {
+    alert('15 minutes remaining!')
+  } else if (m === 10) {
+    alert('10 minutes remaining!')
+  } else if (m === 5) {
+    alert('5 minutes remaining!')
+  } else if (m === 1) {
+    alert('1 minute remaining!')
+  }
+}
+
+function initializeClock (id, endtime) {
+  // Intialize or store deadline in session Storage
+  if (!sessionStorage.getItem('deadline')) {
+    sessionStorage.setItem('deadline', String(deadline))
+  } else {
+    var deadlineString = sessionStorage.getItem('deadline')
+    deadline = new Date(Date.parse(deadlineString))
   }
 
-  startTimer () {
-    var currentTime = new Date()
-    var timeSinceStart = this.startTime - currentTime
-    this.remainTime = this.remainTime - timeSinceStart
+  var clock = document.getElementById(id)
+  var minutesSpan = clock.querySelector('.minutes')
+  var secondsSpan = clock.querySelector('.seconds')
 
-    const m = 1000 * 60
-    if (this.remainTime < m * 15) {
-      alert('15 minutes remaining')
-    } else if (this.remainTime < m * 10) {
-      alert('10 minutes remaining')
-    } else if (this.remainTime < m * 5) {
-      alert('5 minutes remaining')
-    } else if (this.remainTime < m * 1) {
-      alert('1 minute remaining')
-    } else if (this.remainTime < 0) {
-      alert('Timeout!')
-      this.timeout = true
+  function updateClock () {
+    var t = getTimeRemaining(endtime)
+
+    alertTimeRemaining(t)
+
+    minutesSpan.innerHTML = t.minutes
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2)
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval)
     }
-
-    var t = setTimeout(this.startTimer, 500)
   }
 
-  displayTime () {
-    var minutes = Math.floor(this.remainTime / 60000)
-    var seconds = Math.floor((this.remainTime % 60000) / 1000).toFixed(0)
-    return minutes + ':' + (seconds < 10 ? '0' : '') + seconds
-  }
+  updateClock()
+  var timeinterval = setInterval(updateClock, 1000)
+}
+
+window.onload = function () {
+  initializeClock('clockdiv', deadline)
 }
