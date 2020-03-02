cd05b9f4bd28d27e83f3171d3dd3e512c459569d
Shozen Dan <xiaoran.duan@gmail.com>
2020-03-01 22:47:05 -0800

    used a for loop inside the function

--- docs/src/Util.js
+++ docs/src/Util.js
@@ -1,9 +1,11 @@
 /**
  * Retrives a variable from the data base and saves it to the session storage.
- * @param {Object} name the key of the variable to retrieve from database.
+ * @param {Object} name_list A array containing the names of the variable to retrieve from database and save to SessionStorage.
  */
-function saveToSessionStorage (name) {
-  // eslint-disable-next-line no-undef
-  var val = getValue(name)
-  sessionStorage.setItem(name, val)
+function saveToSessionStorage (nameList) {
+  for (var i = 0; i < nameList.length; i++) {
+    // eslint-disable-next-line no-undef
+    var val = getValue(nameList[i])
+    sessionStorage.setItem(nameList[i], val)
+  }
 }
