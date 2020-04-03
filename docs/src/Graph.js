97d20877e71e43cddc0329ab43d25a71b3eddd40
Shozen Dan <xiaoran.duan@gmail.com>
2020-04-04 00:07:43 +0900

    design fix

--- docs/src/Graph.js
+++ docs/src/Graph.js
@@ -206,7 +206,7 @@ function resultComparisonGraph (el, proposed, their) {
         font: { size: 20 },
         showarrow: false,
         text: 'Proposed Budget',
-        x: 0.07,
+        x: 0.05,
         y: 1.2
       },
       {
