f7afebb7f591afd807a31309e612e60f9998ad17
Shozen Dan <xiaoran.duan@gmail.com>
2020-04-03 15:48:56 +0900

    added phase 1 instructions

--- docs/src/index.js
+++ docs/src/index.js
@@ -5,7 +5,7 @@ window.addEventListener('load', () => {
 function displayExpla () {
   const div = document.querySelector('.my-alloc')
   console.log(div)
-  const exp = document.createElement('exp-introduction')
+  const exp = document.createElement('phase1-exp')
 
   exp.explanation = { team: 'A', leader: true }
 
