2364a21cb3e1b98951f132cc80a18388b633a79b
Shozen Dan <xiaoran.duan@gmail.com>
2020-04-07 15:19:36 +0900

    design fix

--- docs/src/Components/vote-buttons.js
+++ docs/src/Components/vote-buttons.js
@@ -12,10 +12,10 @@ class VoteButtons extends HTMLElement {
       <div class="container">
         <div class="row">
           <div class="col-sm">
-            <button type="button" class="btn btn-outline-success btn-block" id="vote-agree" onclick="vote(1)">Agree</button>
+            <button type="button" class="btn btn-outline-success btn-lg btn-block" id="vote-agree" onclick="vote(1)">Agree</button>
           </div>
           <div class="col-sm">
-            <button type="button" class="btn btn-outline-danger btn-block" id="vote-agree" onclick="vote(0)">Disagree</button>
+            <button type="button" class="btn btn-outline-danger btn-lg btn-block" id="vote-agree" onclick="vote(0)">Disagree</button>
           </div>
         </div>
       </div>
